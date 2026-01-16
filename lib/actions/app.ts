8b1e9c390f9b000495f54e04d2b1f8731b6f24f4
Mohammad Faisal <mohammadfaisal.ca@gmail.com>
2026-01-15 23:21:57 -0700

    Revert "Add redirect after GitHub app install"
    This reverts commit f477dd2d6a4033b329533329b74ad48c81ba22ce.

--- lib/actions/app.ts
+++ lib/actions/app.ts
@@ -8,11 +8,6 @@ import { generateState } from "arctic";
 const handleAppInstall = async () => {
 	const state = generateState();
 	const appName = process.env.GITHUB_APP_NAME;
-	const baseUrl = process.env.BASE_URL
-		? process.env.BASE_URL
-		: process.env.VERCEL_PROJECT_PRODUCTION_URL
-			? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-			: "";
 
 	// Log for debugging
 	console.log('GITHUB_APP_NAME:', appName);
@@ -21,12 +16,7 @@ const handleAppInstall = async () => {
 		throw new Error('GITHUB_APP_NAME environment variable is not set');
 	}
 
-	if (!baseUrl) {
-		throw new Error('BASE_URL or VERCEL_PROJECT_PRODUCTION_URL is not set');
-	}
-
-	const redirectUri = `${baseUrl}/settings`;
-	const url = `https://github.com/apps/${appName}/installations/new?state=${encodeURIComponent(state)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+	const url = `https://github.com/apps/${appName}/installations/new?state=${encodeURIComponent(state)}`;
 
 	console.log('Redirecting to:', url);
 
